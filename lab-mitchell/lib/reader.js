d121c793b5722b390e831fc522c820ca97d5c7e3
Mitchell M <mitchellm1618@gmail.com>
2018-01-17 16:57:55 -0800

    renamed data files, modified test and reader

--- lab-mitchell/lib/reader.js
+++ lab-mitchell/lib/reader.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const fs = require('fs');
+let reader = module.exports = {};
 
-fs.reader([], (err, data) => {
+reader.reader = (filePaths, callback) => {
+  let results = [];
+  for (i in arr) {
+    fs.readFile(filePaths[i], (err, data) => {
+      if (err) return console.error(err)
+      let dataText = data.toString();
+      results.push(dataText);
+    });
+  }
+};
 
-})
+
+
+
+
+//ensure that 
